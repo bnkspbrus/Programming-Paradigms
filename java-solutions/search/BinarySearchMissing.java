dc61dcff6feabd730a92a0d3a81fe1018ff6af0e
Barsukov_Nikita <bnkspbrus@gmail.com>
2021-02-24 18:37:57 +0300

    HW-1 HW-1

--- java-solutions/search/BinarySearchMissing.java
+++ java-solutions/search/BinarySearchMissing.java
@@ -6,7 +6,7 @@ public class BinarySearchMissing {
         // Pred: args.length > 0 && элементы args[] целые числа, заданные строками
         // Post: index of the search key, if it is contained in the array || (-(insertion point) - 1)
         // Integer.parseInt() Pred: integer in string Post: integer
-        assert args.length >= 1; // :NOTE: (24.02.2021, MA) -- formal language; remove obsolete parts; move this contract to binsearch part
+        assert args.length >= 1;
         final int x = Integer.parseInt(args[0]);
         final int[] array = new int[args.length - 1];
         // args.length >= 1 && array.length >=0
@@ -20,29 +20,26 @@ public class BinarySearchMissing {
         for (int i = 0; i < array.length; i++) {
             array[i] = Integer.parseInt(args[i + 1]);
         }
-        int index = binSearch(array, x);
-        if (index == array.length || array[index] < x) {
-            index = -index - 1; // :NOTE: (24.02.2021, MA) -- this part must be inside binsearch!
-        }
+        int index = binSearchRecursive(array, 0, array.length - 1, x);
 
         System.out.println(index);
     }
 
-    static int binSearch(final int[] array, final int x) { // :NOTE: (24.02.2021, MA) -- namings! use simillar style for method args!
+    static int binSearchIterative(final int[] array, final int x) {
         // Pred: array.length >= 1
         // Post: res == min{i∣a[i]⩽x} || insertion point
-        assert array.length >= 1; // :NOTE: (24.02.2021, MA) -- out of bounds!! use indices for contract
-        double left = 0, right = array.length - 1;
+        assert array.length >= 1;
+        int left = 0, right = array.length - 1;
         // left == 0 && right >= 0
         if (array[(int) right] > x) {
             // for any i : array[i] > x
-            return array.length;
+            return -array.length - 1;
         }
-        int res = (int) Math.floor((left + right) / 2);
+        int res = (left + right) / 2;
         // Inv: array[left] <= res <= array[right]
         while (left != right /* left < right */) {
             // Cond: left < right
-            if (array[res] <= x) { // :NOTE: (24.02.2021, MA) -- && Inv
+            if (array[res] <= x) {
                 // array[res] <= x
                 right = res; // right decreases
                 // left <= res <= right
@@ -53,34 +50,41 @@ public class BinarySearchMissing {
                 // left <= res <= right
 
             }
-            res = (int) Math.floor((left + right) / 2);
+            //res = (int) Math.floor((left + right) / 2);
             // res decreases, when Cond
+            res = (left + right) / 2;
         }
         // Inv && !Cond
         // left == right == res -- min{i∣a[i]⩽x}
+        if (array[res] < x) {
+            res = -res - 1;
+        }
         return res;
     }
 
-    static int binSearch(final int[] array, double left, double right, final int x) { // :NOTE: (24.02.2021, MA) -- int indices!!
+    static int binSearchRecursive(final int[] array, int left, int right, final int x) {
         // Pred: array.length >= 1 && left <= right
         // Post: res == min{i∣a[i]⩽x} || insertion point
         assert array.length >= 1 && left <= right;
         if (array[(int) right] > x) {
             // for any i : array[i] > x
-            return array.length;
+            return -array.length - 1;
         }
-        int res = (int) left;
+        int res = left;
         if (left != right) {
-            int mid = (int) Math.floor((left + right) / 2); // :NOTE: (24.02.2021, MA) -- remove!!
+            int mid = (left + right) / 2;
             if (array[mid] <= x) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
-            res = binSearch(array, left, right, x);
+            res = binSearchRecursive(array, left, right, x);
         }
         // Inv && !Cond
         // left == right == res
+        if (res >= 0 && array[res] < x) {
+            res = -res - 1;
+        }
         return res;
     }
 }
