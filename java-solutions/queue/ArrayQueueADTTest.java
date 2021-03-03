b86d881d68e4d00ec64ee709193f20ed8845d547
Barsukov_Nikita <bnkspbrus@gmail.com>
2021-03-03 12:48:11 +0300

    HW-2

---
+++ java-solutions/queue/ArrayQueueADTTest.java
@@ -0,0 +1,23 @@
+package queue;
+
+import stack.ArrayStackADT;
+
+import java.util.Arrays;
+
+public class ArrayQueueADTTest {
+    public static void fill(ArrayQueueADT queue) {
+        for (int i = 0; i < 5; i++) {
+            ArrayQueueADT.enqueue(queue, i);
+        }
+    }
+    public static void dump(ArrayQueueADT queue) {
+        while (!ArrayQueueADT.isEmpty(queue)) {
+            System.out.println("element: " + ArrayQueueADT.element(queue) + " dequeue " + ArrayQueueADT.dequeue(queue) + " size: " + ArrayQueueADT.size(queue) + " toArray: " + Arrays.toString(ArrayQueueADT.toArray(queue)));
+        }
+    }
+    public static void main(String[] args) {
+        ArrayQueueADT queue = new ArrayQueueADT();
+        fill(queue);
+        dump(queue);
+    }
+}
---
+++ java-solutions/queue/ArrayQueueTest.java
@@ -0,0 +1,22 @@
+package queue;
+
+import java.util.Arrays;
+
+public class ArrayQueueTest {
+    public static void fill(ArrayQueue queue) {
+        for (int i = 0; i < 5; i++) {
+            queue.enqueue(i);
+        }
+    }
+    public static void dump(ArrayQueue queue) {
+        while (!queue.isEmpty()) {
+            System.out.println("element: " + queue.element() + " dequeue " + queue.dequeue() + " size: " + queue.size() + " toArray: " + Arrays.toString(queue.toArray()));
+        }
+    }
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue();
+        fill(queue);
+        dump(queue);
+    }
+}
+
