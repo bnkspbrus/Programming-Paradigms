9d75f363fc1bd4dfb8a730f83ad179bef870fa97
Barsukov_Nikita <bnkspbrus@gmail.com>
2021-03-10 16:06:41 +0300

    HW-3

--- java-solutions/queue/ArrayQueue.java
+++ java-solutions/queue/ArrayQueue.java
@@ -1,60 +1,67 @@
 package queue;
 
-public class ArrayQueue {
-    private int head, size;// tail;
+public class ArrayQueue extends AbstractQueue {
+    private int head;
     private Object[] elements = new Object[5];
 
-    public void enqueue(Object element) {
-        assert element != null;
-        ensureCapacity(size + 1/* new capacity */);
+//    public void enqueue(Object element) {
+//        assert element != null;
+//        ensureCapacity(size + 1 /* new capacity */);
+//        elements[(head + size) % elements.length] = element;
+//    }
+
+    @Override
+    protected void enqueueImpl(Object element) {
+        ensureCapacity(size + 1 /* new capacity */);
         elements[(head + size) % elements.length] = element;
-        size++;
     }
 
     private void ensureCapacity(int capacity) {
         if (capacity > elements.length) {
-            elements = fillArray(new Object[elements.length * 2]);
+//            Object[] newElements = Arrays.copyOf(Arrays.copyOfRange(elements, head, elements.length), elements.length * 2);
+//            if (head + size > elements.length) {
+//                System.arraycopy(elements, 0, newElements, elements.length - head, head);
+//            }
+//            head = 0;
+//            elements = newElements;
+            elements = fillArrayImpl(new Object[elements.length * 2]);
             head = 0;
         }
     }
 
-    public Object dequeue() {
-        assert size > 0;
-        Object ret = elements[head];
+    @Override
+    protected void remove() {
         elements[head] = null;
         head++;
-        size--;
         head %= elements.length;
-        return ret;
     }
 
-    public Object element() {
-        assert size > 0;
+    @Override
+    protected Object elementImpl() {
         return elements[head];
     }
 
-    public int size() {
-        return size;
-    }
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public void clear() {
+    @Override
+    protected void removeAllImpl() {
         elements = new Object[5];
-        head = size = 0;
+        head = 0;
     }
 
-    public Object[] toArray() {
-        return fillArray(new Object[size]/*tail - head*/);
-    }
-
-    private Object[] fillArray(Object[] array) {
-//        Object[] newElements = new Object[newSize];
+    @Override
+    protected Object[] fillArrayImpl(Object[] array) {
         for (int i = 0; i < size; i++) {
             array[i] = elements[(head + i) % elements.length];
         }
         return array;
     }
+
+//    private Object[] copyArray(int newSize) {
+//        Object[] newElements = new Object[newSize];
+//        for (int i = 0; i < size; i++) {
+//            newElements[i] = elements[(head + i) % elements.length];
+//        }
+//        return newElements;
+//    }
 }
+
