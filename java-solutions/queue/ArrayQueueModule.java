7c18b26c3a5522416761381add23844f3ff5fbc6
Barsukov_Nikita <bnkspbrus@gmail.com>
2021-03-10 16:24:21 +0300

    HW-3

--- java-solutions/queue/ArrayQueueModule.java
+++ java-solutions/queue/ArrayQueueModule.java
@@ -1,44 +1,5 @@
 package queue;
 
-/*
-    Model:
-        [a1, a2, ...an]
-        head -- pointer
-        size -- size of queue
-
-    Inv:
-        tail >= head
-        forall i = head..head + size - 1: a[i] != null
-
-    enqueue(queue, e)
-    Pred: e != null
-    Post: size = size' + 1 && head = head' && a[tail'] = e && forall i = head..head + size - 1: a[i] = a'[i]
-
-    dequeue(queue)
-    Pred: size > 0
-    Post: head = head' + 1 && size == size' - 1 R == a[head'] && i = head..head + size - 1: a[i] = a'[i]
-
-    element(queue)
-    Pred: size > 0
-    Post: R == a[head] && head == head' && size = size' && forall i = head..head + size - 1: a[i] = a'[i]
-
-    size(queue)
-    Pred: true
-    Post: R == size && forall i = head..head + size - 1: a[i] = a'[i] && size = size' && head = head'
-
-    isEmpty(queue)
-    Pred: true
-    Post: R == (size == 0) && forall i = head..head + size - 1: a[i] = a'[i] && head = head' && size = size'
-
-    toArray(queue)
-    Pred: true
-    Post: Object[] array : array.length = size && forall i : 0 <= i <= array.length --> a[i] == i - ый эл-т в очереди && forall i = head..head + size - 1: a[i] = a'[i] size == size' && head == head'
-
-    clear()
-    Pred: true
-    Post: forall i = head..head + size - 1 : a[i] = null && head == size = 0
- */
-
 public class ArrayQueueModule {
     private static int head, size;
     private static Object[] elements = new Object[5];
