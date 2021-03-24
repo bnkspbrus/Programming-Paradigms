7495015962879342c5a6c09f69939b5d8278438a
Barsukov_Nikita <bnkspbrus@gmail.com>
2021-03-24 15:31:03 +0300

    HW-5

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,4 +1,31 @@
-"use strict";
+let binary = func => (left, right) => (x, y, z) => {
+    return func(left(x, y, z), right(x, y, z));
+}
 
+let unary = func => left => (x, y, z) => {
+    return func(left(x, y, z));
+}
 
+let cnst = value => (x, y, z) => value;
+
+let variable = name => (x, y, z) => {
+    switch (name) {
+        case "x":
+            return x;
+        case "y":
+            return y;
+        default:
+            return z;
+    }
+}
+
+let negate = unary(a => -a);
+
+let divide = binary((a, b) => a / b);
+
+let multiply = binary((a, b) => a * b);
+
+let subtract = binary((a, b) => a - b);
+
+let add = binary((a, b) => a + b);
 
