c174735f4595f94412782f16220f12b5f8f10297
Barsukov_Nikita <bnkspbrus@gmail.com>
2021-03-31 16:23:10 +0300

    HW-6

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -47,33 +47,37 @@ Binary.prototype = {
 }
 
 function Add(left, right) {
-    Binary.call(this, left, right);
-    this.sign = "+";
-    this.func = (a, b) => a + b;
+    // Binary.call(this, left, right);
+    // this.sign = "+";
+    // this.func = (a, b) => a + b;
+    initBin.call(this, left, right, "+", (a, b) => a + b);
 }
 
 Add.prototype = Object.create(Binary.prototype);
 
 function Subtract(left, right) {
-    Binary.call(this, left, right);
-    this.sign = "-";
-    this.func = (a, b) => a - b;
+    // Binary.call(this, left, right);
+    // this.sign = "-";
+    // this.func = (a, b) => a - b;
+    initBin.call(this, left, right, "-", (a, b) => a - b);
 }
 
 Subtract.prototype = Object.create(Binary.prototype);
 
 function Multiply(left, right) {
-    Binary.call(this, left, right);
-    this.sign = "*";
-    this.func = (a, b) => a * b;
+    // Binary.call(this, left, right);
+    // this.sign = "*";
+    // this.func = (a, b) => a * b;
+    initBin.call(this, left, right, "*", (a, b) => a * b);
 }
 
 Multiply.prototype = Object.create(Binary.prototype);
 
 function Divide(left, right) {
-    Binary.call(this, left, right);
-    this.sign = "/";
-    this.func = (a, b) => a / b;
+    // Binary.call(this, left, right);
+    // this.sign = "/";
+    // this.func = (a, b) => a / b;
+    initBin.call(this, left, right, "/", (a, b) => a / b);
 }
 
 Divide.prototype = Object.create(Binary.prototype);
@@ -92,25 +96,41 @@ Unary.prototype = {
 }
 
 function Negate(left) {
-    Unary.call(this, left);
-    this.sign = "negate";
-    this.func = a => -a;
+    // Unary.call(this, left);
+    // this.sign = "negate";
+    // this.func = a => -a;
+    initUn.call(this, left, "negate", a => -a);
 }
 
 Negate.prototype = Object.create(Unary.prototype);
 
 function ArcTan(left) {
-    Unary.call(this, left);
-    this.sign = "atan";
-    this.func = a => Math.atan(a);
+    // Unary.call(this, left);
+    // this.sign = "atan";
+    // this.func = a => Math.atan(a);
+    initUn.call(this, left, "atan", a => Math.atan(a));
 }
 
 ArcTan.prototype = Object.create(Unary.prototype);
 
 function ArcTan2(left, right) {
-    Binary.call(this, left, right);
-    this.sign = "atan2";
-    this.func = (a, b) => Math.atan2(a, b);
+    // Binary.call(this, left, right);
+    // this.sign = "atan2";
+    // this.func = (a, b) => Math.atan2(a, b);
+    initBin.call(this, left, right, "atan2", (a, b) => Math.atan2(a, b));
 }
 
 ArcTan2.prototype = Object.create(Binary.prototype);
+
+function initBin(left, right, sign, func) {
+    Binary.call(this, left, right);
+    this.sign = sign;
+    this.func = func;
+}
+
+function initUn(left, sign, func) {
+    Unary.call(this, left);
+    this.sign = sign;
+    this.func = func;
+}
+
